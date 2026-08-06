154a836bb9a1ad4eb525384932c3a4d878c69e5b
Arpit Tagade <143588157+tagadearpit@users.noreply.github.com>
2026-08-06 17:36:56 +0530

    Add documentation comment to cn utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Utility to merge Tailwind CSS classes with proper conflict resolution.
+ * Combines clsx for conditional classes and tailwind-merge for deduplication.
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
